1279d0f52ee9f4f6e2b696c1dd9a2f18430f2244
Katie Truong <katietruong.ucdavis@gmail.com>
2018-07-20 14:56:50 -0700

    Set up basic server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const db = require('../db/index.js')
 const PORT = 5002;
 
 app.use(bodyParser.json());
@@ -9,7 +10,17 @@ app.use(bodyParser.urlencoded({extended: true}));
 // Server static assests
 app.use(express.static(__dirname + '/../client/dist'));
 
-
+// Design API to look up item by ID:
+app.get('/items/:itemID'), (req, res) => {
+  db.getItembyID(req.params.itemID, (err, results) => {
+    if (err) {
+      console.log('Error sending get to API', err);
+    } else {
+      res.json(results);
+    }
+  });
+  res.json('I got here!');
+};
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
