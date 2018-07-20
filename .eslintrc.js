a1c9aa959c776c63a4b03a8598dbeb955ad1a28e
Katie Truong <katietruong.ucdavis@gmail.com>
2018-07-20 11:33:19 -0700

    Set up webpack and jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,8 @@
-/**
- * These rules enforce the Hack Reactor Style Guide
- *
- * Visit this repo for more information:
- *   https://github.com/reactorcore/eslint-config-hackreactor
- */
-
-module.exports = {
-  extends: './node_modules/eslint-config-hackreactor/index.js'
-};
+{
+ "linters": {
+  "extends": "airbnb",
+   "eslint": {
+       "env": {"PATH":"/usr/local/bin/"}
+   }
+ }
+}
